90011eada7d2ba91fcbba69b690ea95e044269be
Juventino Romero <jromero@nearsoft.com>
2019-05-27 12:32:23 -0700

    add get todo list endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/todo', function(req, res, next) {
+  const todoList = [
+    {name: 'Learn Python', isDone: true},
+    {name: 'Learn AI math', isDone: false},
+    {name: 'Make a basic API project', isDone: true},
+    {name: 'Learn Django', isDone: true},
+    {name: 'Learn Flask', isDone: true},
+    {name: 'Be a pro Python dev', isDone: false},
+    {name: 'Be rich', isDone: false}
+  ]
+
+  res.send(todoList)
+})
+
 module.exports = router;

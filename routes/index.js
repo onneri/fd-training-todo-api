db7f539d2a246c83a5d2497b9fd71b5e3e7b95ab
francisco gerardo neri andriano <fneri@nearsoft.com>
2019-05-27 19:16:22 -0500

    get dummy todos

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/todo', function(req, res, next) {
+router.get('/todos', function(req, res, next) {
   const todoList = [
-    {name: 'Learn Python', isDone: true},
-    {name: 'Learn AI math', isDone: false},
-    {name: 'Make a basic API project', isDone: true},
-    {name: 'Learn Django', isDone: true},
-    {name: 'Learn Flask', isDone: true},
-    {name: 'Be a pro Python dev', isDone: false},
-    {name: 'Be rich', isDone: false}
+    {id:1, title: 'Learn Python', done: true},
+    {id:2, title: 'Learn AI math', done: false},
+    {id:3, title: 'Make a basic API project', done: true},
+    {id:4, title: 'Learn Django', done: true},
+    {id:5, title: 'Learn Flask', done: true},
+    {id:6, title: 'Be a pro Python dev', done: false},
+    {id:7, title: 'Be rich', done: false}
   ]
 
   res.send(todoList)
